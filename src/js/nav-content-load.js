ec8f2ab34c0ccce96b935879d56b5fb3dd7c1ddd
AviyaCG <aviyacg@gmail.com>
2022-10-26 14:27:54 +0300

    Add navAddProject function add a project button to the projects list

--- src/js/nav-content-load.js
+++ src/js/nav-content-load.js
@@ -13,6 +13,26 @@ export function navContentLoad(){
 
     const CREATE_PROJECT = document.createElement('button');
     CREATE_PROJECT.id = 'create-project-button';
-    CREATE_PROJECT.textContent = 'Create Project';
+    CREATE_PROJECT.textContent = '+ New Project';
     PROJECTS_LIST.appendChild(CREATE_PROJECT);
+}
+
+export function navAddProject(project){
+    let button = document.createElement('button');
+    button.classList.add('nav-project-button');
+    button.textContent = project.getTitle();
+    addElementToProjectList(button);
+}
+
+function addElementToProjectList(elemnet){
+    const PROJECTS_LIST = getProjectList();
+    let listNode = document.createElement('li');
+    listNode.appendChild(elemnet);
+    PROJECTS_LIST.appendChild(listNode);
+}
+
+function getProjectList(){
+    const NAV = document.querySelector('#nav');
+    const PROJECTS_LIST = NAV.childNodes.item(1);
+    return PROJECTS_LIST;
 }
