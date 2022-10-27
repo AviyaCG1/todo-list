2844ca641315a415c7306794f8b8a20467a08726
AviyaCG <aviyacg@gmail.com>
2022-10-27 19:18:33 +0300

    Edit mainAddProjectCard - Add ADD_TASK button

--- src/js/main-content-load.js
+++ src/js/main-content-load.js
@@ -2,6 +2,7 @@ import { Project } from "./project";
 
 export function myProjectsLoad(projects){
     const MAIN_CONTENT = getMainContent();
+    MAIN_CONTENT.classList.add('my-projects');
 
     projects.forEach(project => mainAddProjectCard(project));
 }
@@ -22,12 +23,17 @@ export function mainAddProjectCard(project){
     project.getTaskList().forEach(task => {
         CARD.appendChild(createCardTask(task))
     });
+
+    const ADD_TASK = document.createElement('button');
+    ADD_TASK.classList.add('add-task-button');
+    ADD_TASK.textContent = '+ Add task';
+    CARD.appendChild(ADD_TASK);
 }
 
 // create a task div for project card display
 function createCardTask(task){
     const TASK = document.createElement('div');
-    TASK.classList.add('.task');
+    TASK.classList.add('task');
 
     const TASK_TITLE = document.createElement('div');
     TASK_TITLE.classList.add('task-title');
@@ -35,7 +41,7 @@ function createCardTask(task){
     TASK.appendChild(TASK_TITLE);
 
     const CHECKBOX = document.createElement('div');
-    CHECKBOX.classList.add('checkbox', task.isCompleted() ? 'checked' : 'unchecked');
+    CHECKBOX.classList.add('checkbox', task.isCompleted() ? 'checked' : null);
     TASK.appendChild(CHECKBOX);
 
     return TASK;
