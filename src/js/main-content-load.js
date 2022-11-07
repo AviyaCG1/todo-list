56cd3835d9b6066f48c2348ce9ba48897c84f0f0
AviyaCG <aviyacg@gmail.com>
2022-11-07 18:55:49 +0200

    Create function singleProjectLoad

--- src/js/main-content-load.js
+++ src/js/main-content-load.js
@@ -2,8 +2,9 @@ import { Project } from "./project";
 
 export function myProjectsLoad(projects){
     const MAIN_CONTENT = getMainContent();
+    MAIN_CONTENT.classList.remove('single-project')
     MAIN_CONTENT.classList.add('my-projects');
-
+    MAIN_CONTENT.replaceChildren();
     projects.forEach(project => mainAddProjectCard(project));
 }
 
@@ -30,7 +31,62 @@ export function mainAddProjectCard(project){
     CARD.appendChild(ADD_TASK);
 }
 
-// create a task div for project card display
+export function mainRemoveProject(project){
+    const CARD = document.querySelector(`#main-content [data-project="${project.getTitle()}"]`);
+    CARD.remove();
+}
+
+export function singleProjectLoad(project){
+    const MAIN_CONTENT = getMainContent();
+    MAIN_CONTENT.classList.remove('my-projects')
+    MAIN_CONTENT.classList.add('single-project');
+    MAIN_CONTENT.replaceChildren();
+
+    const TITLE = document.createElement('div');
+    TITLE.classList.add('project-title');
+    TITLE.textContent = project.getTitle();
+    MAIN_CONTENT.appendChild(TITLE);
+
+    project.getTaskList().forEach(task => addTask(task));
+
+    const ADD_TASK = document.createElement('button');
+    ADD_TASK.classList.add('add-task-button');
+    ADD_TASK.textContent = '+ Add task';
+    MAIN_CONTENT.appendChild(ADD_TASK);
+}
+
+function addTask(task){
+    const MAIN_CONTENT = getMainContent();
+    const TASK = document.createElement('div');
+    TASK.classList.add('task');
+
+    const TASK_TITLE = document.createElement('div');
+    TASK_TITLE.classList.add('task-title');
+    TASK_TITLE.textContent = task.getTitle();
+    TASK.appendChild(TASK_TITLE);
+
+    const TASK_DESCRIPTION = document.createElement('div');
+    TASK_DESCRIPTION.classList.add('task-description');
+    TASK_DESCRIPTION.textContent = task.description;
+    TASK.appendChild(TASK_DESCRIPTION);
+
+    const TASK_DATE = document.createElement('div');
+    TASK_DATE.classList.add('task-date');
+    TASK_DATE.textContent = task.dueDate;
+    TASK.appendChild(TASK_DATE);
+
+    const TASK_PRIORITY = document.createElement('div');
+    TASK_PRIORITY.classList.add('task-priority');
+    TASK_PRIORITY.textContent = task.priority;
+    TASK.appendChild(TASK_PRIORITY);
+
+    const CHECKBOX = document.createElement('div');
+    CHECKBOX.classList.add('checkbox', task.isCompleted() ? 'checked' : null);
+    TASK.appendChild(CHECKBOX);
+
+    MAIN_CONTENT.appendChild(TASK);
+}
+
 function createCardTask(task){
     const TASK = document.createElement('div');
     TASK.classList.add('task');
@@ -45,11 +101,6 @@ function createCardTask(task){
     TASK.appendChild(CHECKBOX);
 
     return TASK;
-}
-
-export function mainRemoveProject(project){
-    const CARD = document.querySelector(`#main-content [data-project="${project.getTitle()}"]`);
-    CARD.remove();
 }
 
 function getMainContent(){
