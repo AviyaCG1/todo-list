40382f986392e7fbca2fe59fe67944837f04692d
AviyaCG <aviyacg@gmail.com>
2022-11-08 21:01:19 +0200

    cleanup

--- src/js/main-content-load.js
+++ src/js/main-content-load.js
@@ -1,44 +1,4 @@
-import { Project } from "./project";
-
-export function myProjectsLoad(projects) {
-  const MAIN_CONTENT = getMainContent();
-  MAIN_CONTENT.classList.remove("single-project");
-  MAIN_CONTENT.classList.add("my-projects");
-  MAIN_CONTENT.replaceChildren();
-  projects.forEach((project) => mainAddProjectCard(project));
-}
-
-export function mainAddProjectCard(project) {
-  const MAIN_CONTENT = getMainContent();
-
-  const CARD = document.createElement("div");
-  CARD.classList.add("card");
-  CARD.dataset.project = project.getTitle();
-  MAIN_CONTENT.appendChild(CARD);
-
-  const TITLE = document.createElement("div");
-  TITLE.classList.add("card-title");
-  TITLE.textContent = project.getTitle();
-  CARD.appendChild(TITLE);
-
-  project.getTaskList().forEach((task) => {
-    CARD.appendChild(createCardTask(task));
-  });
-
-  const ADD_TASK = document.createElement("button");
-  ADD_TASK.classList.add("add-task-button");
-  ADD_TASK.textContent = "+ Add task";
-  CARD.appendChild(ADD_TASK);
-}
-
-export function mainRemoveProject(project) {
-  const CARD = document.querySelector(
-    `#main-content [data-project="${project.getTitle()}"]`
-  );
-  CARD.remove();
-}
-
-export function singleProjectLoad(project) {
+export default function projectLoad(project) {
   const MAIN_CONTENT = getMainContent();
   MAIN_CONTENT.classList.remove("my-projects");
   MAIN_CONTENT.classList.add("single-project");
@@ -90,25 +50,6 @@ function addTask(task) {
   TASK.appendChild(CHECKBOX);
 
   MAIN_CONTENT.appendChild(TASK);
-}
-
-function createCardTask(task) {
-  const TASK = document.createElement("div");
-  TASK.classList.add("task");
-
-  const TASK_TITLE = document.createElement("div");
-  TASK_TITLE.classList.add("task-title");
-  TASK_TITLE.textContent = task.getTitle();
-  TASK.appendChild(TASK_TITLE);
-
-  const CHECKBOX = document.createElement("div");
-  CHECKBOX.classList.add(
-    "checkbox",
-    task.isCompleted() ? "checked" : "unchecked"
-  );
-  TASK.appendChild(CHECKBOX);
-
-  return TASK;
 }
 
 function getMainContent() {
--- src/js/nav-content-load.js
+++ src/js/nav-content-load.js
@@ -1,79 +1,62 @@
-import { Project } from './project';
-import { pubsub } from './pub-sub';
+import Project from "./project";
+import pubsub from "./pub-sub";
 
 export function navContentLoad(projects) {
-  const NAV = document.querySelector('#nav');
+  const NAV = document.querySelector("#nav");
 
-  const MY_PROJECTS = document.createElement('button');
-  MY_PROJECTS.id = 'my-projects';
-  MY_PROJECTS.textContent = 'My Projects';
-  NAV.appendChild(MY_PROJECTS);
+  projects.forEach((project) => addProject(project));
 
-  const PROJECTS_LIST = document.createElement('ul');
-  PROJECTS_LIST.id = 'projects-list';
-  NAV.appendChild(PROJECTS_LIST);
-
-  projects.forEach((project) => navAddProject(project));
-
-  const CREATE_PROJECT = document.createElement('button');
-  CREATE_PROJECT.id = 'create-project-button';
-  CREATE_PROJECT.textContent = '+ New Project';
-  CREATE_PROJECT.addEventListener('click', newProjectForm);
+  const CREATE_PROJECT = document.createElement("button");
+  CREATE_PROJECT.id = "new-project-button";
+  CREATE_PROJECT.textContent = "+ New Project";
+  CREATE_PROJECT.addEventListener("click", newProjectForm);
   NAV.appendChild(CREATE_PROJECT);
 }
 
 function newProjectForm() {
-  const FORM = document.createElement('div');
-  FORM.id = 'new-project-form';
+  const FORM = document.createElement("div");
+  FORM.id = "new-project-form";
 
-  const INPUT = document.createElement('input');
-  INPUT.placeholder = 'e.g buy stuff';
+  const INPUT = document.createElement("input");
+  INPUT.placeholder = "e.g buy stuff";
   FORM.appendChild(INPUT);
 
-  const CREATE = document.createElement('button');
-  CREATE.id = 'create';
-  CREATE.textContent = 'Create';
-  CREATE.addEventListener('click', (e) => {
-    const title = document.querySelector('#new-project-form input').value;
-    if (title === '') return;
+  const CREATE = document.createElement("button");
+  CREATE.id = "create";
+  CREATE.textContent = "Create";
+  CREATE.addEventListener("click", (e) => {
+    const title = document.querySelector("#new-project-form input").value;
+    if (title === "") return;
 
     const newProject = new Project(title);
-    pubsub.publish('ADD_PROJECT', newProject);
+    pubsub.publish("ADD_PROJECT", newProject);
   });
   FORM.appendChild(CREATE);
 
-  const CANCEL = document.createElement('button');
-  CANCEL.id = 'cancel';
-  CANCEL.textContent = 'cancel';
+  const CANCEL = document.createElement("button");
+  CANCEL.id = "cancel";
+  CANCEL.textContent = "cancel";
   FORM.appendChild(CANCEL);
 
   // replace the button with the form
-  document.querySelector('#create-project-button').remove();
-  document.querySelector('#nav').appendChild(FORM);
+  document.querySelector("#create-project-button").remove();
+  document.querySelector("#nav").appendChild(FORM);
 }
 
-export function navAddProject(project) {
-  const button = document.createElement('button');
-  button.classList.add('nav-project-button');
-  button.textContent = project.getTitle();
-  button.dataset.project = project.getTitle();
-  addElementToProjectList(button);
+export function addProject(project) {
+  const BUTTON = document.createElement("button");
+  BUTTON.classList.add("nav-project-button");
+  BUTTON.textContent = project.getTitle();
+  BUTTON.dataset.project = project.getTitle();
+
+  document
+    .querySelector("#new-project-buttton")
+    .previousElementSibling.append(BUTTON);
 }
 
 export function navRemoveProject(project) {
   const button = document.querySelector(
-    `#nav [data-project="${project.getTitle()}"]`,
+    `#nav [data-project="${project.getTitle()}"]`
   );
   button.parentNode.remove();
-}
-
-function addElementToProjectList(element) {
-  const PROJECTS_LIST = getProjectList();
-  const listNode = document.createElement('li');
-  listNode.appendChild(element);
-  PROJECTS_LIST.appendChild(listNode);
-}
-
-function getProjectList() {
-  return document.querySelector('#projects-list');
 }
